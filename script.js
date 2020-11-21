b59758b7b31cd23ea816666b3972b3560396da05
Sanzhar <semid46@gmail.com>
2020-11-21 22:27:06 +0600

    simple movie app

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert(1)
+const numberOfFilms = +prompt("Сколько вы посмотрели фильмов?", '0');
+
+const personalMovie = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let filmName = prompt("Один из последний просмотренных фильмов?", "");
+let filmRate = +prompt("На сколько оцените его?", "");
+
+personalMovie.movie[filmName] = filmRate;
+
+console.log(personalMovie);

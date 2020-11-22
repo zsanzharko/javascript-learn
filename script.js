f107655738a8876bb9744a0a67eab73581611db1
Sanzhar <semid46@gmail.com>
2020-11-22 14:00:29 +0600

    movie with functions

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 let filmName;
 let filmRate;
 let numberOfFilms = +prompt("Сколько вы посмотрели фильмов?", '0');
-
-while (numberOfFilms <= 0) {
-    alert("Error input");
-    numberOfFilms = +prompt("Сколько вы посмотрели фильмов?", '0');
-    if (numberOfFilms > 0) {
-        break;
-    }
-}
-if (numberOfFilms > 0 && numberOfFilms < 10) {
-    alert("Просмотрено довольно мало...");
-} else if (numberOfFilms < 30) {
-    alert("Вы классичесикй зритель...");
-} else alert("Вы киноман :D")
-
 const personalMovie = {
     count: numberOfFilms,
     movie: {},
@@ -23,16 +9,46 @@ const personalMovie = {
     privat: false
 };
 
-filmName = prompt("Один из последний просмотренных фильмов?", "");
-while (filmName.length === 0 || filmName.length >= 50){
-    alert("Error input");
-    filmName = prompt("Один из последний просмотренных фильмов?", "");
-}
-filmRate = +prompt("На сколько оцените его?", "");
-while (filmRate.length === 0 || filmRate.length >= 50){
-    alert("Error input");
-    filmRate = prompt("Один из последний просмотренных фильмов?", "");
-}
-personalMovie.movie[filmName] = filmRate;
+start();
+detectPersonalLevel();
+rememberMyFilm();
+showMyDB(personalMovie.privat)
 
-console.log(personalMovie);
+function start() {
+    while (numberOfFilms === ''|| numberOfFilms == null) {
+        numberOfFilms = +prompt("Сколько вы посмотрели фильмов?", '0');
+    }
+}
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovie)
+    }
+}
+
+function writeYourGenres() {
+    for (let number = 1; number <= 3; number++) {
+        personalMovie.genres[number - 1] = +prompt(`Ваш любимый жанр под номером ${number}`);
+    }
+}
+function detectPersonalLevel() {
+    if (numberOfFilms > 0 && numberOfFilms < 10) {
+        alert("Просмотрено довольно мало...");
+    } else if (numberOfFilms < 30) {
+        alert("Вы классичесикй зритель...");
+    } else alert("Вы киноман :D")
+}
+
+function rememberMyFilm() {
+    filmName = prompt("Один из последний просмотренных фильмов?", "");
+    while (filmName.length === 0 || filmName.length >= 50) {
+        alert("Error input");
+        filmName = prompt("Один из последний просмотренных фильмов?", "");
+    }
+    filmRate = +prompt("На сколько оцените его?", "");
+    while (filmRate.length === 0 || filmRate.length >= 50) {
+        alert("Error input");
+        filmRate = prompt("Один из последний просмотренных фильмов?", "");
+    }
+    personalMovie.movie[filmName] = filmRate;
+}
